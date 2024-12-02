81d36276e5f2e2b03800122b0b7a351e915a4034
Shikhar Dixit <siyadesignby@gmail.com>
2024-12-02 16:53:50 +0530

    Your commit message here

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,37 @@
-import React, { useRef } from 'react' 
+import React, { useRef } from 'react';
 import CustomHook from './CustomHook';
-function Home() {
-const scrollTab = useRef();
-CustomHook(scrollTab);
 
-return (
+function Home() {
+  const scrollTab = useRef();
+  CustomHook(scrollTab);
+
+  return (
     <section ref={scrollTab} className='home'>
-        <div className="content">
-            <div className="name">
-                I AM  <span>PRIYA</span>
-            </div>
-            <div className="des">
-                {/* 30 */}
-                Graphic Designer | Content Creator | Script Writer | Freelancer <br />                 
-                 "Designs can be Art. Designs can be Aesthethic. Designs is so Simple, thats Why it is so Complicated."
-            </div>
-            
-            <a href="/democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active '>
-                Download My CV
-            </a>
+      <div className="content">
+        <div className="name">
+          I AM <span>PRIYA</span>
         </div>
-        <div className="avatar">
-            <div className="card">
-                <img src="/avatar.png" alt="" />
-                <div className="info">
-                    <div>Graphic Designer</div>
-                    <div>Indian</div>
-                    <div>@designsby_siya</div>
-                    <div>Freelancer</div>
-                </div>
-            </div>
+        <div className="des">
+          Graphic Designer | Content Creator | Script Writer | Freelancer <br />
+          "Designs can be Art. Designs can be Aesthetic. Designs are so Simple, that's Why it is so Complicated."
         </div>
+        <a href="/democv.pdf" target="_blank" rel="noopener noreferrer" className='animation active'>
+          Download My CV
+        </a>
+      </div>
+      <div className="avatar">
+        <div className="card">
+          <img src="/avatar.PNG" alt="Avatar" />
+          <div className="info">
+            <div>Graphic Designer</div>
+            <div>Indian</div>
+            <div>@designsby_siya</div>
+            <div>Freelancer</div>
+          </div>
+        </div>
+      </div>
     </section>
-    )
+  );
 }
 
-export default Home
+export default Home;
