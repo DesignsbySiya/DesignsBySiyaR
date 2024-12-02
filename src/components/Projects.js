afb52ec564b85c88d6bed5b81ce0270074ef03a6
DesignsbySiya <siyadesignby@gmail.com>
2024-12-02 17:11:05 +0530

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -8,20 +8,20 @@ function Projects() {
       name: 'LinkedIn Carousels',
       des: 'In this vibrant and engaging carousel, Siya showcases five simple yet powerful hacks to spark creativity and generate innovative ideas. The design features a character in a suit giving a thumbs-up, adding a friendly and approachable touch. With a bold and modern gradient background transitioning from dark to light red, the visual appeal is strong. The key points, including “5 DONTs,” are highlighted within circular icons, making the information easy to digest.',
       Tools: 'Adobe Photoshop',
-      images: '/project1.JPG',
+      images: 'project1.JPG',
       instagramLink: 'https://www.instagram.com/p/DCzO_Dyzgw4/?utm_source=ig_web_copy_link'
     },
     {
       name: 'Bai Bubbles - Peru Pineapples',
       des: 'In this striking advertisement, the Bai Bubbles can takes center stage, proudly showcasing its refreshing Peru Pineapple flavor. The cans sleek design highlights key features such as being a "Sparkling Antioxidant Infusion" and having just "5 calories." Surrounding the can are vibrant images of pineapple slices, whole pineapples, and splashes of bright yellow liquid, evoking the tropical and invigorating essence of the drink. The addition of green leaves enhances the fresh and natural appeal, making the design both lively and appetizing.',
       Tools: 'Canva Premium',
-      images: '/project2.PNG',
+      images: 'project2.PNG',
       instagramLink: 'https://www.instagram.com/p/DDEjnn2zK6K/?igsh=MXBwNGhrZDB0ZzBwZQ=='
     },
     {
       name: 'The Game Of Life',
       des: 'In this Magnificient Project I have Shown the game of Life through the pieces of Chess Board In Creative Style and these styles are created with help of AI Image Generation And Canva Premium ',
-      images: '/project3.JPG',
+      images: 'project3.JPG',
       instagramLink: 'https://www.instagram.com/p/DC1-CS6T7AG/?igsh=MWI5bTA0NXUzandubQ=='
     }
   ]);
